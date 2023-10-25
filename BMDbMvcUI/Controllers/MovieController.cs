bdace46c90351c91f416fce53e674c7012c58207
Bahram <baxram1997007@gmail.com>
2023-10-25 15:34:16 +0400

    Remove pagination from  MVC project for final

--- BMDbMvcUI/Controllers/MovieController.cs
+++ BMDbMvcUI/Controllers/MovieController.cs
@@ -7,37 +7,19 @@ public class MovieController : Controller
 {
     private readonly IAsyncMovieService _service;
 
-    public MovieController(IAsyncMovieService service)
-        => _service = service;
+    public MovieController(IAsyncMovieService asyncMovieService)
+        => _service = asyncMovieService;
 
     [HttpGet]
-    public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
+    public async Task<IActionResult> Index()
     {
-        var movies = await _service.GetMoviesAsync();
-        var paginatedMovies = movies.Skip((page - 1) * pageSize).Take(pageSize);
-        var totalMovies = movies.Count;
-
-        ViewBag.TotalPages = (int)Math.Ceiling(totalMovies / (double)pageSize);
-        ViewBag.CurrentPage = page;
-
-        return View(paginatedMovies);
+        return await Task.FromResult<IActionResult>(View(await _service.GetMoviesAsync()));
     }
 
-
-    [HttpPost]
-    public async Task<IActionResult> SearchMoviesAsync(string search, int page = 1, int pageSize = 10)
+    public async Task<IActionResult> SearchMoviesAsync()
     {
-        if (page <= 0) throw new ArgumentOutOfRangeException(nameof(page));
-        var searchResults = await _service.SearchMoviesAsync(search);
-        var paginatedSearchResults = searchResults.Skip((page - 1) * pageSize).Take(pageSize);
-        var totalResults = searchResults.Count;
-
-        ViewBag.TotalPages = (int)Math.Ceiling(totalResults / (double)pageSize);
-        ViewBag.CurrentPage = page;
-
-        return View("Index", paginatedSearchResults);
+        return await Task.FromResult<IActionResult>(View());
     }
-
 
     [HttpPost]
     public async Task<IActionResult> SearchMoviesAsync(string search)
