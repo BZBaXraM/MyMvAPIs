d61fe963771a3e275849b090e9302853d6cf54ee
Bahram <baxram1997007@gmail.com>
2024-03-01 00:36:51 +0400

    update unit

--- BMDb.UnitTests/UnitTest1.cs
+++ BMDb.UnitTests/UnitTest1.cs
@@ -71,4 +71,50 @@ public class UnitTest1
         Assert.Equal(movie.Year, addedMovie.Year);
         Assert.Equal(movie.Plot, addedMovie.Plot);
     }
+
+    [Fact]
+    public async Task Get_Movies_Test()
+    {
+        // Arrange
+        var movies = new List<Movie>
+        {
+            new()
+            {
+                Director = "Serdar Akar",
+                Genre = "Action",
+                Poster = null,
+                Trailer = null,
+                Title = "Kurtlar Vadisi Irak",
+                Year = "2006",
+                Plot = null,
+                Id = Guid.NewGuid()
+            },
+            new Movie
+            {
+                Director = "Serdar Akar",
+                Genre = "Action",
+                Poster = null,
+                Trailer = null,
+                Title = "Kurtlar Vadisi Irak",
+                Year = "2006",
+                Plot = null,
+                Id = Guid.NewGuid()
+            }
+        };
+
+        var dbContextMock = new DbContextMock<MovieContext>(
+            new DbContextOptionsBuilder<MovieContext>().Options
+        );
+
+        dbContextMock.CreateDbSetMock(x => x.Movies, movies);
+
+        var movieService = new MovieService(dbContextMock.Object);
+
+        // Act
+        var result = await movieService.GetMoviesAsync("1", "10", "title");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
+    }
 }
